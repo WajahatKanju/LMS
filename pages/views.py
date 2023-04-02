1218327f782c8d26fda35673a0106fb94d475a4b
Wajahat Kanju <wajahat5ahmad@gmail.com>
2023-04-03 02:19:44 +0500

    add cards

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from django.views.generic import View
 
 from main.form import SettingsForm
@@ -19,15 +20,31 @@ class HomeView(View):
 
 class SettingsView(View):
     template_name = 'pages/settings.html'
+    success_url = reverse_lazy('settings')
 
     # form_class = SettingsForm()
     context = {
+        'form': SettingsForm(),
+        'errors': [],
     }
 
     def get(self, request):
 
         setting = Settings.objects.filter(employee__id__exact=self.request.user.id).first()
-        form_class = SettingsForm(instance=setting)
-        self.context['form'] = form_class
+        form = SettingsForm(instance=setting)
+        self.context['form'] = form
         print(self.request.user.id)
         return render(request, self.template_name, self.context)
+
+    def post(self, request,):
+        setting = Settings.objects.filter(employee__id__exact=self.request.user.id).first()
+        form = SettingsForm(request.POST, instance=setting)
+        if form.is_valid():
+            form.save(commit=False)
+            form.employee = self.request.user
+            form.save()
+
+            return redirect(self.success_url)
+
+        self.context['errors'].append({'type': 'error Type', 'message': 'Invalid'})
+        return redirect(self.success_url)
